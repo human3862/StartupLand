3f22c27cf8d3bd018152827f7c83a830055270d1
human <skuranov09@gmail.com>
2026-01-16 23:43:46 +1200

    фы

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20260111_024516 from './20260111_024516';
 import * as migration_20260111_024624 from './20260111_024624';
 import * as migration_20260111_081958 from './20260111_081958';
 import * as migration_20260116_014009_add_pricing_block from './20260116_014009_add_pricing_block';
+import * as migration_20260116_114222 from './20260116_114222';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20260116_014009_add_pricing_block.up,
     down: migration_20260116_014009_add_pricing_block.down,
-    name: '20260116_014009_add_pricing_block'
+    name: '20260116_014009_add_pricing_block',
+  },
+  {
+    up: migration_20260116_114222.up,
+    down: migration_20260116_114222.down,
+    name: '20260116_114222'
   },
 ];
